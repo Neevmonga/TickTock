e079381b20846d3665ce2ec4bca3536b8b7c679f
Neevmonga <72914539+Neevmonga@users.noreply.github.com>
2021-01-01 21:48:46 +0530

    Add files via upload

---
+++ Clock.js
@@ -0,0 +1,13 @@
+class Clock {
+    constructor(x,y,width,height) {
+       
+        
+      }
+      display(){
+        push();
+        ellipseMode(CENTER);
+        ellipse(this.ellipse, 0, 0, 493,493);
+        pop();
+      }
+    };
+  
